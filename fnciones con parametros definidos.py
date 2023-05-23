91fca5e5412dfea20a8c8c28c563ff724fdc141b
Agustina Aperlo <43002670+magustinaperlo@users.noreply.github.com>
2023-05-23 18:50:39 -0300

    Update_fnciones con parametros definidos.py

--- fnciones con parametros definidos.py
+++ fnciones con parametros definidos.py
@@ -23,3 +23,31 @@ def perimetro_circulo (b,a=math.pi):          #funcion para calcular el perimetr
 def volumen_cilindro (c,a=math.pi, b= 52):
     resultado = a*b**2*c
     return resultado
+
+#sugerencia: 
+
+import math
+
+# FUNCIONES CON PARÁMETROS DEFINIDOS
+
+def division(a, b=5):
+    resultado = a / b
+    return resultado
+
+def promedio(a, b, c, d=3):
+    suma = a + b + c
+    resultado = suma / d
+    return resultado
+
+def porcentaje(a, b=25, c=100):
+    resultado = (b * a) / c
+    return resultado
+
+def perimetro_circulo(radio, pi=math.pi):
+    resultado = 2 * pi * radio
+    return resultado
+
+def volumen_cilindro(altura, radio=52, pi=math.pi):
+    resultado = pi * radio**2 * altura
+    return resultado
+se reemplaza "//" por "/" para obtener mas decimales 
